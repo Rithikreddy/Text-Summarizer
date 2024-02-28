778231c86a12cb5ca2583ea66874576c6225e0a0
Rithik Reddy <rithikreddy@Rithiks-MacBook-Air.local>
2024-02-28 13:34:58 -0800

    Data validation Completed

--- src/textsummarizer/config/configuration.py
+++ src/textsummarizer/config/configuration.py
@@ -1,7 +1,6 @@
 from textsummarizer.constants import *
 from textsummarizer.utils.common import read_yaml,create_directories
-from textsummarizer.entity import DataIngestionConfig
-
+from textsummarizer.entity import DataIngestionConfig, DataValidationConfig
 #step 4: updating the configuration manager.
 class ConfigurationManager:
     def __init__(
@@ -27,3 +26,15 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir = config.root_dir,
+            STATUS_FILE = config.STATUS_FILE,
+            ALL_REQUIRED_FILES = config.ALL_REQUIRED_STATUS
+        )
+        return data_validation_config
